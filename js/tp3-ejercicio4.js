fc090142db08ca20d3e16d0181b66f3d0cbcc01d
Lucas Capdevila <lcapdevila60@gmail.com>
2023-11-07 22:39:43 -0300

    ejercicio 4 resuelto

---
+++ js/tp3-ejercicio4.js
@@ -0,0 +1,19 @@
+//* FUNCIONES
+
+/*
+4- Escribir el código de una función a la que se pasa como parámetro un número entero 
+y devuelve como resultado una cadena de texto que indica si el número es par o impar. 
+Mostrar por pantalla el resultado devuelto por la función.
+*/
+
+const num = parseInt(prompt('Ingrese un número'))
+
+const numero = (num) => {
+  if(num%2 === 0){
+    document.write(`El número ${num} es par`)
+  } else{
+    document.write(`El número ${num} es impar`)
+  }
+}
+
+numero(num)
